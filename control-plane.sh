e7cf2eb66ee08078d60b3b3e31f2f2fa503dc1cc
Danyi <99331148+DnyKristof@users.noreply.github.com>
2025-04-14 21:16:24 +0200

    Update control-plane.sh

--- control-plane.sh
+++ control-plane.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sysctl net.ipv4.ip_forward=1
 kubeadm init --apiserver-advertise-address=$(hostname -I) --pod-network-cidr=10.244.0.0/16
 
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
