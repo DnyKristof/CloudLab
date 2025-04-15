0996f0974e7f987b03b5f37d2184e7f258f84d22
Danyi <99331148+DnyKristof@users.noreply.github.com>
2025-04-15 10:45:20 +0200

    Update argocd_install.sh

--- argocd_install.sh
+++ argocd_install.sh
@@ -3,10 +3,16 @@ kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
 kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
+
+kubectl -n argocd patch secret argocd-secret \
+    -p '{"stringData": {"admin.password": "$2a$10$mivhwttXM0U5eBrZGtAG8.VSRL1l9cZNAmaSaqotIzXRBRwID1NT.",
+        "admin.passwordMtime": "'$(date +%FT%T)'"
+    }}'
+
 echo "Access: "
 kubectl get svc -n argocd argocd-server
 echo "Waiting for service to load"
 sleep 30
 echo "Username: admin"
-echo "Password : $(kubectl get secret argocd-initial-admin-secret -n argocd -o jsonpath="{.data.password}" | base64 -d && echo) "
+echo "Password : admin"
 
